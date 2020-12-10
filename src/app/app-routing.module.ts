87f57d69eaca25ae2c180d8a452aa4cd89301a01
Xenofon Vourliotis <x.vourliotis@gmail.com>
2020-12-10 11:36:52 +0200

    added other routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateBugComponent } from './bugs/create-bug/create-bug.component';
+import { EditBugComponent } from './bugs/edit-bug/edit-bug.component';
 import { ContentComponent } from './wrapper/content/content.component';
 
 const routes: Routes = [
-  { path: "", component : ContentComponent}
+  { path: "", component : ContentComponent},
+  { path: "create-bug", component : CreateBugComponent},
+  { path: "edit-bug/:id", component : EditBugComponent}
 ];
 
 @NgModule({
