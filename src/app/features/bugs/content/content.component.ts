7131bdb655dd06b78093cf1a8f21c3c6b5a1641f
Xenofon Vourliotis <x.vourliotis@gmail.com>
2020-12-17 05:40:00 +0200

    formatted code

--- src/app/features/bugs/content/content.component.ts
+++ src/app/features/bugs/content/content.component.ts
@@ -3,95 +3,97 @@ import { Router } from '@angular/router';
 import { Bugs } from '../models/bugs.model';
 import { BugsService } from '../services/bugs.service';
 
-
-
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+  styleUrls: ['./content.component.scss'],
 })
 export class ContentComponent implements OnInit {
-
   arrayOfBugs: Bugs[];
   pageNumber = 0;
   returnPage = 0;
   increaseEnabled = true;
-  
+
   currentSort = {
     order: false,
-    currentCategory: "none"
-  }
+    currentCategory: 'none',
+  };
 
-  constructor(private bugs: BugsService, private router: Router) { }
+  constructor(private bugs: BugsService, private router: Router) {}
 
   ngOnInit(): void {
     this.bugs.getBugsByPage(this.pageNumber).subscribe((data) => {
       this.arrayOfBugs = data;
-    })
-    this.checkNextPage()
+    });
+    this.checkNextPage();
   }
 
-  sortBugs(category:string) {
-
-    if(this.currentSort.currentCategory == category){
-      if(this.currentSort.order == false){
-        this.currentSort.order = true
-      }else{
-        this.currentSort.order = false
+  sortBugs(category: string) {
+    if (this.currentSort.currentCategory == category) {
+      if (this.currentSort.order == false) {
+        this.currentSort.order = true;
+      } else {
+        this.currentSort.order = false;
       }
-    }else{
-      this.currentSort.order = true
-      this.currentSort.currentCategory = category
+    } else {
+      this.currentSort.order = true;
+      this.currentSort.currentCategory = category;
     }
 
     this.bugs.getBugs(this.currentSort.order, category).subscribe((data) => {
       this.arrayOfBugs = data;
-    })
-  }
-
-  arrowStyle(category:string){
-    if(this.currentSort.currentCategory == category && this.currentSort.order == false){
-      return "fa-sort-down"
-    }else if(this.currentSort.currentCategory == category && this.currentSort.order == true){
-      return "fa-sort-up"
-    }else{
-      return ""
-    }
-  }
-
-  deleteBug(id:string){
-    this.bugs.deleteBug(id).subscribe(data => {
-      this.arrayOfBugs = this.arrayOfBugs.filter(item => item.id !== id);
     });
   }
 
-  changePage(direction: string){
-    if(direction == "increase"){
-      this.pageNumber += 1
-    }else if(direction == "decrease"){
-      this.pageNumber -= 1
+  arrowStyle(category: string) {
+    if (
+      this.currentSort.currentCategory == category &&
+      this.currentSort.order == false
+    ) {
+      return 'fa-sort-down';
+    } else if (
+      this.currentSort.currentCategory == category &&
+      this.currentSort.order == true
+    ) {
+      return 'fa-sort-up';
+    } else {
+      return '';
+    }
+  }
+
+  deleteBug(id: string) {
+    this.bugs.deleteBug(id).subscribe((data) => {
+      this.arrayOfBugs = this.arrayOfBugs.filter((item) => item.id !== id);
+    });
+  }
+
+  changePage(direction: string) {
+    if (direction == 'increase') {
+      this.pageNumber += 1;
+    } else if (direction == 'decrease') {
+      this.pageNumber -= 1;
     }
     this.bugs.getBugsByPage(this.pageNumber).subscribe((data) => {
-      if( (data === undefined || data.length == 0) &&  direction == "increase"){
-        this.pageNumber -= 1
-        this.returnPage = this.pageNumber
+      if ((data === undefined || data.length == 0) && direction == 'increase') {
+        this.pageNumber -= 1;
+        this.returnPage = this.pageNumber;
         this.increaseEnabled = false;
         return;
       }
       this.arrayOfBugs = data;
-    })
-    this.checkNextPage()
-    this.returnPage = this.pageNumber
+    });
+    this.checkNextPage();
+    this.returnPage = this.pageNumber;
   }
 
-  checkNextPage(){
+  checkNextPage() {
     let nextPage = this.pageNumber + 1;
     this.bugs.getBugsByPage(nextPage).subscribe((data) => {
-      if( (data === undefined || data.length == 0)){
+      if (data === undefined || data.length == 0) {
         this.increaseEnabled = false;
         return;
       }
       this.increaseEnabled = true;
-    })
+    });
   }
 }
--- src/app/features/bugs/create-bug/create-bug.component.ts
+++ src/app/features/bugs/create-bug/create-bug.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { delay } from 'rxjs/operators';
 import { BugsService } from '../services/bugs.service';
@@ -8,24 +14,25 @@ import { FormValidationService } from '../services/form-validation.service';
 @Component({
   selector: 'app-create-bug',
   templateUrl: './create-bug.component.html',
-  styleUrls: ['./create-bug.component.scss']
+  styleUrls: ['./create-bug.component.scss'],
 })
-export class CreateBugComponent implements OnInit{
+export class CreateBugComponent implements OnInit {
   //commented out also ^^ implements OnDestroy
 
-  createForm: FormGroup
+  createForm: FormGroup;
   // bugsSubscription: Subscription
 
-  get comments(){
-    return this.createForm.get('comments') as FormArray
+  get comments() {
+    return this.createForm.get('comments') as FormArray;
   }
 
   constructor(
-    private fb: FormBuilder, 
-    private bugsService: BugsService, 
-    private router: Router, 
-    private formValidationService: FormValidationService) { }
-  
+    private fb: FormBuilder,
+    private bugsService: BugsService,
+    private router: Router,
+    private formValidationService: FormValidationService
+  ) {}
+
   ngOnInit(): void {
     this.createForm = this.fb.group({
       title: [null, Validators.required],
@@ -33,34 +40,40 @@ export class CreateBugComponent implements OnInit{
       priority: [null, Validators.required],
       reporter: [null, Validators.required],
       status: [null],
-      comments: this.fb.array([])
-    })
+      comments: this.fb.array([]),
+    });
   }
-
 
   //function also called in html , use of insert 0 to push comment on the top
   addComment() {
-    this.comments.insert(0,(this.fb.group({reporter:"", description:""})))
+    this.comments.insert(0, this.fb.group({ reporter: '', description: '' }));
   }
-  removeComment(index:number){
+  removeComment(index: number) {
     this.comments.removeAt(index);
   }
 
-  ValidateField(formInput:string){
+  ValidateField(formInput: string) {
     //adds and Removes validation of QA input
     this.formValidationService.addRemoveValidationsOfQA(this.createForm);
     //returns CSS Bootstrap class "is-valid" or "is-invalid"
-    return this.formValidationService.CSSinputValidation(this.createForm, formInput);
+    return this.formValidationService.CSSinputValidation(
+      this.createForm,
+      formInput
+    );
   }
 
-  formSubmit(): void{
-    if (!this.createForm.valid){
+  formSubmit(): void {
+    if (!this.createForm.valid) {
       //if not valid >> touches all inputs for validation
-     return this.formValidationService.touchAllFormFields(this.createForm)}
-     // Posts form data to server after 100 ms delay
-    else this.bugsService.postBug(this.createForm.value).pipe(delay(100)).subscribe(response => {
-      this.router.navigate([""])
-    })
+      return this.formValidationService.touchAllFormFields(this.createForm);
+    }
+    // Posts form data to server after 100 ms delay
+    else
+      this.bugsService
+        .postBug(this.createForm.value)
+        .pipe(delay(100))
+        .subscribe((response) => {
+          this.router.navigate(['']);
+        });
   }
-
 }
